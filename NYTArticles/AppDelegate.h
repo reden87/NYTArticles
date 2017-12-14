413d105fdeafd23fa5093a34dec1a4705127ae91
Gergo Nemeth <gergo.nemeth@supercharge.io>
2017-12-14 16:51:24 +0100

    Initial Commit

---
+++ NYTArticles/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NYTArticles
+//
+//  Created by Gergo Nemeth on 2017. 12. 14..
+//  Copyright © 2017. Gergo Nemeth. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NYTArticles/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  NYTArticles
+//
+//  Created by Gergo Nemeth on 2017. 12. 14..
+//  Copyright © 2017. Gergo Nemeth. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
